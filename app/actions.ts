7fe37c7837b260d613d997ddb5ccd85ec8c5ebea
Akshit4340 <24ce01005@iitbbs.ac.in>
2026-03-26 10:21:17 +0530

    feat: server side actions

---
+++ app/actions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import z from "zod/v3";
+import { blogSchema } from "./schemas/blog";
+import { fetchMutation } from "convex/nextjs";
+import { api } from "@/convex/_generated/api";
+import { redirect } from "next/navigation";
+import { getToken } from "@/lib/auth-server";
+
+export const createBlogAction = async (values: z.infer<typeof blogSchema>) => {
+  const parsed = blogSchema.safeParse(values);
+  if (!parsed.success) {
+    throw new Error("Failed to parse data");
+  }
+
+  const token = await getToken();
+  if (!token) {
+    throw new Error("Unauthorized");
+  }
+  await fetchMutation(api.posts.createPost, {
+    title: parsed.data.title,
+    body: parsed.data.content,
+  }, {token});
+
+  return redirect("/")
+};
